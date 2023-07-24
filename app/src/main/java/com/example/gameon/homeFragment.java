f6f90c3ed60aa939850c2a47f4d6db2d4899fd29
ManithLD <manithlakvindu.d@gmail.com>
2023-07-23 20:25:28 -0400

    Added search by title search bar to the workout layout

--- app/src/main/java/com/example/gameon/homeFragment.java
+++ app/src/main/java/com/example/gameon/homeFragment.java
@@ -2,17 +2,26 @@ package com.example.gameon;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.AggregateQuery;
+import com.google.firebase.firestore.AggregateQuerySnapshot;
+import com.google.firebase.firestore.AggregateSource;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -26,6 +35,7 @@ public class homeFragment extends Fragment {
     FirebaseFirestore firebaseFirestore;
     workoutAdapter adapter;
     StaggeredGridLayoutManager staggeredGridLayoutManager;
+    private SearchView searchView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,7 +47,51 @@ public class homeFragment extends Fragment {
         firebaseFirestore = FirebaseFirestore.getInstance();
         setUpRecycler();
 
+        searchView = root.findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterItems(newText);
+                return false;
+            }
+        });
+
         return root;
+    }
+
+    private void filterItems(String text) {
+        /*
+        Query query = firebaseFirestore.collection("workouts")
+                .document(firebaseUser.getUid()).collection("myWorkouts")
+                .whereArrayContains("tags", text).orderBy("title", Query.Direction.ASCENDING);
+         */
+        //Toast.makeText(this.getContext(), text, Toast.LENGTH_SHORT).show();
+
+        if (!text.isEmpty()) {
+            Query query = firebaseFirestore.collection("workouts")
+                    .document(firebaseUser.getUid()).collection("myWorkouts")
+                    .whereEqualTo("title", text).orderBy("title", Query.Direction.ASCENDING);
+            FirestoreRecyclerOptions<firebasemodel> filteredWorkouts = new FirestoreRecyclerOptions
+                    .Builder<firebasemodel>().setQuery(query, firebasemodel.class).build();
+            adapter = new workoutAdapter(filteredWorkouts, this.getContext());
+            adapter.startListening();
+            recyclerView.setAdapter(adapter);
+        } else {
+            Query query = firebaseFirestore.collection("workouts")
+                    .document(firebaseUser.getUid()).collection("myWorkouts")
+                    .orderBy("title", Query.Direction.ASCENDING);
+            FirestoreRecyclerOptions<firebasemodel> filteredWorkouts = new FirestoreRecyclerOptions
+                    .Builder<firebasemodel>().setQuery(query, firebasemodel.class).build();
+            adapter = new workoutAdapter(filteredWorkouts, this.getContext());
+            adapter.startListening();
+            recyclerView.setAdapter(adapter);
+        }
     }
 
     private void setUpRecycler() {
